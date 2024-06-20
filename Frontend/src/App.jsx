86a858b25f55672c122b3110f8e24e924a3ba219
keorgtz <kevin12ortizgt@gmail.com>
2024-06-20 12:31:18 -0600

    Page Routing Complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
-
+import {MyRoutes} from "./routes/routes"
+import styled from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import {Sidebar} from "./components/Sidebar"
 function App() {
-
   return (
-  <div>
-    <h1> Hola </h1>
-  </div>
-  )
+    <div>
+        <BrowserRouter>
+            <Container>
+              <Sidebar/>
+                <MyRoutes/>
+            </Container>
+        </BrowserRouter>
+    </div>
+  );
 }
-
+const Container = styled.div``;
 export default App
---
+++ Frontend/src/routes/routes.jsx
@@ -0,0 +1,20 @@
+import {Routes, Route} from "react-router-dom"
+import {Home} from "../pages/Home"
+import {Clients} from "../pages/Clients"
+import {Configuration} from "../pages/Configuration"
+import {Products} from "../pages/Products"
+import {Reports} from "../pages/Reports"
+import {Users} from "../pages/Users"
+
+export function MyRoutes(){
+    return(
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/configuration" element={<Configuration/>}/>
+                    <Route path="/clients" element={<Clients/>}/>
+                    <Route path="/products" element={<Products/>}/>
+                    <Route path="/reports" element={<Reports/>}/>
+                    <Route path="/users" element={<Users/>}/>
+                </Routes>
+    );
+}
