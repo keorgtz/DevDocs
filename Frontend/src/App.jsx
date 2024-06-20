8e0ec473d6c305acddd8c5dc8f6495017a026d80
keorgtz <kevin12ortizgt@gmail.com>
2024-06-20 16:00:54 -0600

    Make Themes & Set Structure Themes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,34 @@
-import {MyRoutes} from "./routes/routes"
+import { MyRoutes } from "./routes/routes";
 import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
-import {Sidebar} from "./components/Sidebar"
+import { Sidebar } from "./components/Sidebar";
+import React from "react";
+import { Light, Dark } from "./styles/Themes";
+
+export const ThemeContext = React.createContext(null);
 function App() {
+  const [theme, setTheme] = React.useState("light");
+  const themeStyle = theme === "light" ? Light : Dark;
+
   return (
     <div>
-        <BrowserRouter>
-            <Container>
-              <Sidebar/>
-                <MyRoutes/>
-            </Container>
-        </BrowserRouter>
+      <ThemeContext.Provider value={{ setTheme, theme }}>
+
+        <ThemeProvider theme={themeStyle}>
+
+          <BrowserRouter>
+          <Container>
+            <Sidebar />
+            <MyRoutes />
+          </Container>
+          </BrowserRouter>
+          
+        </ThemeProvider>
+
+      </ThemeContext.Provider>
     </div>
   );
 }
 const Container = styled.div``;
-export default App
+export default App;
