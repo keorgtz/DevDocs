b6413d67d0620e7d1dc8147d28cd75768330af52
keorgtz <kevin12ortizgt@gmail.com>
2024-06-21 16:13:13 -0600

    starting sidebar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,56 @@
+//importando dependencias funciones y componentes asi como los themas claros y oscuros
 import { MyRoutes } from "./routes/routes";
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar";
 import React from "react";
+import { useState } from "react";
 import { Light, Dark } from "./styles/Themes";
 
+//Creando el contexto del tema
 export const ThemeContext = React.createContext(null);
+//Creando la app
 function App() {
-  const [theme, setTheme] = React.useState("light");
+  //Creando el estado del tema
+  const [theme, setTheme] = useState("light");
+  //Variable para cambiar el tema
   const themeStyle = theme === "light" ? Light : Dark;
 
+  //Funcion para cambiar el tema
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+  };
+  //Funcion para saber si el estado de el Sidebar esta expandido o contraido
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
+    //Creando el componente
     <div>
       <ThemeContext.Provider value={{ setTheme, theme }}>
-
         <ThemeProvider theme={themeStyle}>
-
           <BrowserRouter>
-          <Container>
-            <Sidebar />
-            <MyRoutes />
-          </Container>
+            <Container className={sidebarOpen ? "sidebarState active" : ""}>
+              <Sidebar
+                sidebarOpen={sidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+              />
+              <MyRoutes />
+            </Container>
           </BrowserRouter>
-          
         </ThemeProvider>
-
       </ThemeContext.Provider>
     </div>
   );
 }
-const Container = styled.div``;
+
+//Estilos del contenerdor para el sidebar
+const Container = styled.div`
+    dispklay: grid;
+    grid-template-columns: 90px auto;
+    background: ${({ theme }) => theme.bgtotal};
+    &.active {
+      grid-template-columns: 300px auto;
+    }
+`;
 export default App;
--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,8 +1,18 @@
+import styled from "styled-components";
 
-export function Sidebar(){
+
+export function Sidebar({ sidebarOpen, setSidebarOpen }){
     return(
-        <div>
-            <h1>Sidebar</h1>
-        </div>
+        <Container>
+            <div className="LogoContent">
+
+            </div>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    color: ${(props) => props.theme.text};
+
+    background: ${(props) => props.theme.bg};
+`;
